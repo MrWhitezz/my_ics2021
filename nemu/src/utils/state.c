b73a6e99d97df264f3e9251b5c896a404740282b
tracer-ics2021 <tracer@njuics.org>
2021-09-19 17:23:10 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 5bf5cd0164e24fd586010658f6245eb4   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  17:23:10 up  8:13,  1 user,  load average: 1.67, 1.32, 1.12

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -4,7 +4,7 @@ NEMUState nemu_state = { .state = NEMU_STOP };
 
 int is_exit_status_bad() {
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
-    (nemu_state.state == NEMU_STOP);
-    //(nemu_state.state == NEMU_QUIT);
+    (nemu_state.state == NEMU_STOP);// to use 'q' directly quit elegently
+    //(nemu_state.state == NEMU_QUIT); 
   return !good;
 }
