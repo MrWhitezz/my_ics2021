208c8337e70495445752e8fb4c124894d5859510
tracer-ics2021 <tracer@njuics.org>
2021-09-22 17:40:33 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 6842bc8a73874fd2b13e3227ad13708a   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  17:40:33 up  3:00,  1 user,  load average: 0.48, 0.36, 0.21

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -99,8 +99,11 @@ static int cmd_x(char *args){
   paddr_t mem_addr = (paddr_t)cmd_p(mem_expr);
   for (int i = 0; i < num; ++i){
     paddr_t mem_visit = mem_addr + (paddr_t)i * 4;
-    printf("0x%8x: %8x\n", mem_visit, paddr_read(mem_visit, 4));
-    //printf("mem_read %d times with location: %8x\n", i, mem_visit);
+    //word_t mem_val = paddr_read(mem_visit, 4);
+    printf("0x%8x:\t", mem_visit);
+    for (int j = 0; j < 4; ++j)
+      printf("%2x ", paddr_read(mem_visit + j, 1));
+    printf("\n");
   }
   
   
