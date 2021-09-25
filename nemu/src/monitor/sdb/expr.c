64957f625dd536f934ca51e52e5abf551be43fbf
tracer-ics2021 <tracer@njuics.org>
2021-09-25 16:24:33 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: c6864da7eb8e471c84e8298669fddf64   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  16:24:33 up  2:09,  1 user,  load average: 0.72, 0.43, 0.37

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -121,8 +121,12 @@ word_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
-  if (nr_token == 1 && tokens[0].type == TK_NOTYPE){
+  if (nr_token == 0 && tokens[0].type == TK_NOTYPE){
     Log("String with only spaces found");
+    return 0;
+  }
+  if (nr_token >= 32){
+    Log("The expression is too long");
     return 0;
   }
 
@@ -132,14 +136,74 @@ word_t expr(char *e, bool *success) {
   return eval(0, nr_token - 1);
 }
 
+bool check_parentheses(int p, int q){
+    assert(p < q && q < 32 && p >= 0);
+    if (tokens[p].type != '(' || tokens[q].type != ')')
+      return false;
+      
+    int parenthese_num = 1;
+    for (int i = p + 1; i <= q; ++i){
+      if (tokens[i].type == '(') ++parenthese_num;
+      else if (tokens[i].type == ')') --parenthese_num;
+      
+      if (parenthese_num == 0)
+        return false;
+    }
+    if (parenthese_num != 0){
+      Log("Very Bad Expression");
+      return false;
+    }
+    return true;
+}
+int find_op(int p, int q){
+  enum {P_NUM = 1, P_MUL_OR_DIV, P_ADD_OR_SUB};
+  int parentheses_num = 0; int cur_precedence = 0; int cur_op = p;
+  for (int i = p; i <= q; ++i){
+    if (tokens[i].type == '(') ++parentheses_num;
+    else if (tokens[i].type == ')') --parentheses_num;
+    else if (parentheses_num == 0){
+      int i_precedence = 0;
+      if (tokens[i].type == TK_NUM) i_precedence = P_NUM;
+      if (tokens[i].type == '+' || tokens[i].type == '-') i_precedence = P_ADD_OR_SUB;
+      if (tokens[i].type == '*' || tokens[i].type == '/') i_precedence = P_MUL_OR_DIV;
+
+      if (i_precedence >= cur_precedence){
+        cur_precedence = i_precedence;
+        cur_op = i;
+      }
+
+    }
+  }
+  return cur_op;
+}
+
 word_t eval (int p, int q){
     if (p > q){
       Log("This expression is bad");
       return 0;
     }
     else if (p == q){
-      assert(tokens[p].type == TK_NUM);
+      if (tokens[p].type != TK_NUM){
+        Log("This expression is bad and strange");
+        return 0;
+      }
       return atoi(tokens[p].str);
+    }
+    else if (check_parentheses(p ,q))
+        return eval(p + 1, q - 1);
+    else{
+      int op = find_op(p ,q);
+      int val1 = eval(p, op - 1);
+      int val2 = eval(op + 1, q);
+      switch (tokens[op].type)
+      {
+      case '+': return val1 + val2;
+      case '-': return val1 - val2;
+      case '*': return val1 * val2;
+      case '/': return val1 / val2;
+      
+      default: Log("Bad op"); break;
+      }
     }
     return 0;
 }
