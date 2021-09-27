5f9d2414f154382d99328a4b0485f0aa784af66a
tracer-ics2021 <tracer@njuics.org>
2021-09-27 22:34:53 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 957ac8cc39bd4af5a5c0b11690997027   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  22:34:53 up  3:56,  1 user,  load average: 0.60, 0.77, 0.80

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -6,7 +6,7 @@
 #include <string.h>
 
 // this should be enough
-static char buf[65536] = {};
+static char buf[65536 + 10] = {};
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
 "#include <stdio.h>\n"
@@ -15,9 +15,61 @@ static char *code_format =
 "  printf(\"%%u\", result); "
 "  return 0; "
 "}";
+int pos_buf = 0;
+static unsigned choose(unsigned n){
+    //srand((unsigned)time(0));
+    return rand() % n;
+}
+// static void gen_rand_expr() {
+//   buf[0] = '\0';
+// }
+static unsigned gen_num(){
+    assert(pos_buf < 65536);
+    //srand((unsigned)time(0));
+    unsigned num = (rand() % 10); assert(num >= 0 && num <= 9);
+    buf[pos_buf++] = num + '0'; buf[pos_buf] = '\0';
+    return num;    
+}
+static void gen(char ch){
+    assert(pos_buf < 65536);
+    buf[pos_buf++] = ch; buf[pos_buf] = '\0';
+    return;
+}
+static char gen_rand_op(){
+    switch(choose(4)){
+      case 0: assert(pos_buf < 65536); buf[pos_buf++] = '+'; return '+'; 
+      case 1: assert(pos_buf < 65536); buf[pos_buf++] = '-'; return '-'; 
+      case 2: assert(pos_buf < 65536); buf[pos_buf++] = '*'; return '*'; 
+      case 3: assert(pos_buf < 65536); buf[pos_buf++] = '/'; return '/'; 
+      default: printf("Cannot generate random op!\n");
+    }
+}
+static int gen_rand_expr() {
+  switch (choose(3)) {
 
-static void gen_rand_expr() {
-  buf[0] = '\0';
+    case 0: {
+            gen('('); 
+            unsigned res = gen_rand_expr(); 
+            gen(')'); 
+            //printf("buf = %s\n", buf);
+            assert(pos_buf < 65536); buf[pos_buf] = '\0';
+            return res; break;}
+    case 1: {  unsigned val1; val1 = gen_rand_expr(); 
+                char ch; ch = gen_rand_op(); 
+                unsigned val2; val2 = gen_rand_expr();
+                //if(val2 == 0) {pos_buf = 0; return gen_rand_expr();}
+                assert(pos_buf < 65536); buf[pos_buf] = '\0';
+                switch(ch){
+                  case '+': return val1 + val2;
+                  case '-': return val1 - val2;
+                  case '*': return val1 * val2;
+                  case '/': return val1 / val2;
+                  default: printf("Cannot return gen_rand_expr\n");}
+             }
+    default: {int num = gen_num(); 
+              assert(pos_buf < 65536); buf[pos_buf] = '\0';
+              return num; break;}
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -29,8 +81,9 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
+    pos_buf = 0;
     gen_rand_expr();
-
+    printf("buf = %s\n", buf);
     sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
