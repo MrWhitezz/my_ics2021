135f63796b19524ca92cd7a77f67568b851af481
tracer-ics2021 <tracer@njuics.org>
2021-09-27 18:54:02 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 957ac8cc39bd4af5a5c0b11690997027   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  18:54:02 up 15 min,  1 user,  load average: 1.22, 1.03, 0.67

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -115,7 +115,7 @@ static int cmd_x(char *args){
 static int cmd_p(char *args){
   //TODO
     bool is_success = true;
-    printf("%d", expr(args, &is_success));
+    printf("%d\n", expr(args, &is_success));
     return strtol(args + 2, NULL, 16);
 }
 
