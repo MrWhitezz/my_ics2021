e1b4393f2bf6c2e82ec4f1e5b450a0b086a08ae1
tracer-ics2021 <tracer@njuics.org>
2021-09-28 13:37:57 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 3d2e9330905d4fe183360557b1c687f3   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  13:37:57 up 21 min,  1 user,  load average: 0.36, 0.53, 0.47

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -12,16 +12,16 @@ int main(int argc, char *argv[]) {
 #else
   init_monitor(argc, argv);
 #endif
-  // FILE *fp = fopen("/home/xyq/ics2021/nemu/tools/gen-expr/input", "r");
-  // assert(fp != NULL);
-  // char  buff[500] = {}; unsigned res = 0, times = 0; bool success = true;
-  // while (fscanf(fp, "%u", &res) && fscanf(fp, "%s", buff)){
-  //   unsigned my_res = expr(buff, &success);
-  //   if (my_res != res)
-  //     Log("Wrong found! res = %u\tmy_res = %u\texpr = %s", res, my_res, buff);
-  //   else printf("Correct %d times\n", ++times);
-  //   memset(buff, 0, sizeof(buff));
-  // }
+  FILE *fp = fopen("/home/xyq/ics2021/nemu/tools/gen-expr/input", "r");
+  assert(fp != NULL);
+  char  buff[500] = {}; unsigned res = 0, times = 0; bool success = true;
+  while (fscanf(fp, "%u", &res) && fscanf(fp, "%s", buff)){
+    unsigned my_res = expr(buff, &success);
+    if (my_res != res)
+      Log("Wrong found! res = %u\tmy_res = %u\texpr = %s", res, my_res, buff);
+    else printf("Correct %d times\n", ++times);
+    memset(buff, 0, sizeof(buff));
+  }
 
   /* Start engine. */
   engine_start();
