ff904e4544b290732501f05c4456958ff52a0303
tracer-ics2021 <tracer@njuics.org>
2021-09-28 13:44:56 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 3d2e9330905d4fe183360557b1c687f3   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  13:44:56 up 28 min,  1 user,  load average: 0.48, 0.48, 0.48

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -16,11 +16,11 @@ int main(int argc, char *argv[]) {
   assert(fp != NULL);
   char  buff[500] = {}; unsigned res = 0, times = 0; bool success = true;
   while (fscanf(fp, "%u", &res) && fscanf(fp, "%s", buff)){
-    if (buff[0] == '\0') break;
+    if (buff[0] == '\0') {printf("Correct %d times\n", times);break;}
     unsigned my_res = expr(buff, &success);
     if (my_res != res)
       Log("Wrong found! res = %u\tmy_res = %u\texpr = %s", res, my_res, buff);
-    else printf("Correct %d times\n", ++times);
+    else ++times;
     memset(buff, 0, sizeof(buff));
   }
 
