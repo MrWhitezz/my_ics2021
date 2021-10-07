0a497a570634a9094bd19bffcea8ef9e11960f59
tracer-ics2021 <tracer@njuics.org>
2021-10-07 20:06:36 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: d1bbb7915fbd4c77b986e4424f8e3b94   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  20:06:36 up 2 days,  3:48,  1 user,  load average: 0.85, 0.63, 0.49

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -23,11 +23,11 @@ void sdb_mainloop();
 void fetch_decode(Decode *s, vaddr_t pc);
 // I don't whether this trace_and_difftest is right or not
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
-// #ifdef CONFIG_ITRACE_COND
-//   if (ITRACE_COND) log_write("%s\n", _this->logbuf);
-// #endif
-//   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
-//   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+#ifdef CONFIG_ITRACE_COND
+  if (ITRACE_COND) log_write("%s\n", _this->logbuf);
+#endif
+  if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
+  IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   // if (examine_wp()){
   //   nemu_state.state = NEMU_STOP;
   //   Log("there's watchpoint changing\n");
