e85e36567d980fd3fa421b961801a8e49a62125a
tracer-ics2021 <tracer@njuics.org>
2021-10-13 21:31:15 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: ecc373b16f094d139a52eac7fbacaa56   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  21:31:15 up 1 day,  4:04,  5 users,  load average: 0.42, 0.36, 0.32

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -3,7 +3,11 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  if (cpu.pc != pc) return false;
+  for (int i = 0; i < 32; ++i)
+    if (gpr(i) != ref_r->gpr[i]._32)
+      return false;
+  return true;
 }
 
 void isa_difftest_attach() {
