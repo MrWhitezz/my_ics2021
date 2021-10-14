4571969d983f6a3658323c22e5b5ca63eb4e6a71
tracer-ics2021 <tracer@njuics.org>
2021-10-14 19:09:21 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: ad3628983a47477e8888c67307b668c5   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  19:09:22 up  9:13,  5 users,  load average: 0.44, 0.23, 0.29

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,15 +5,28 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t len = 0;
+  for (; s[len] != '\0'; ++len) {}
+  return len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  //while(*dst++ = *strsrc++);
+  size_t i = 0;
+  for (; src[i] != '\0'; ++i)
+    dst[i] = src[i];
+  dst[i] = '\0'; 
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  size_t i;
+  for (i = 0; i < n && src[i] != '\0'; ++i){
+    dst[i] = src[i];
+  }
+  for (; i < n; ++i)
+    dst[i] = '\0';
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
