5490c4711c4a31108b1cf93f303576ccd5915f24
tracer-ics2021 <tracer@njuics.org>
2021-10-14 16:27:32 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: ad3628983a47477e8888c67307b668c5   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  16:27:32 up  6:31,  5 users,  load average: 0.51, 0.44, 0.29

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -2,7 +2,7 @@
 #include "../local-include/rtl.h"
 
 #define INSTR_LIST(f) f(add) f(sub) f(slt) f(sltu) f(xor) f(or) \
-f(mul) f(div) \
+f(mul) f(mulu) f(div) \
 f(addi) f(sltiu) f(andi) f(slli) f(srai) f(lui) f(auipc) \
 f(jal) f(jalr) f(lw) f(sw) f(beq) f(bne) f(blt) f(bge) f(bltu) \
 f(inv) f(nemu_trap)
--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -54,6 +54,10 @@ def_EHelper(mul) {
   rtl_mulu_lo(s, ddest, dsrc1, dsrc2);
 }
 
+def_EHelper(mulu) {
+  rtl_muls_hi(s, ddest, dsrc1, dsrc2);
+}
+
 def_EHelper(div) {
   rtl_divs_q(s, ddest, dsrc1, dsrc2);
 }
