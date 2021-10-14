9eaf65a9638a751432c6a088a73d9105530b1dee
tracer-ics2021 <tracer@njuics.org>
2021-10-14 11:58:52 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: ad3628983a47477e8888c67307b668c5   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  11:58:52 up  2:03,  5 users,  load average: 1.17, 0.56, 0.41

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -42,6 +42,10 @@ def_EHelper(sltiu) {
   rtl_setrelopi(s, RELOP_LTU, ddest, id_src1->preg, id_src2->imm);
 }
 
+def_EHelper(slli) {
+  rtl_slli(s, ddest, dsrc1, id_src2->imm & 0x1f);
+}
+
 def_EHelper(srai) {
   rtl_srai(s, ddest, dsrc1, id_src2->imm & 0x1f); // here to get shamt(5 bits) we need to & 0b11111
 }
