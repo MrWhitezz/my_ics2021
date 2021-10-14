59ba1aa5dcb97d11909b977011acde009d0a169a
tracer-ics2021 <tracer@njuics.org>
2021-10-14 10:19:16 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: ad3628983a47477e8888c67307b668c5   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  10:19:16 up 23 min,  4 users,  load average: 0.57, 0.51, 0.38

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -1,7 +1,7 @@
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
 
-#define INSTR_LIST(f) f(add) \
+#define INSTR_LIST(f) f(add) f(sub) \
 f(addi) f(sltiu) f(lui) f(auipc) \
 f(jal) f(jalr) f(lw) f(sw) f(beq) f(bne) \
 f(inv) f(nemu_trap)
--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -2,6 +2,10 @@ def_EHelper(add) {
   rtl_add(s, ddest, dsrc1, dsrc2);
 }
 
+def_EHelper(sub) {
+  rtl_sub(s, ddest, dsrc1, dsrc2);
+}
+
 def_EHelper(lui) {
   rtl_li(s, ddest, id_src1->imm);
 }
