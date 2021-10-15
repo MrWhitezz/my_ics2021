edd20e52cbd13cdf394083f0f8307e7ba1855a53
tracer-ics2021 <tracer@njuics.org>
2021-10-15 15:46:51 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 45359ecb29074e049a761155cbf399ad   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  15:46:51 up 26 min,  3 users,  load average: 0.31, 0.54, 0.47

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,16 +5,135 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+enum flag_itoa {
+    FILL_ZERO = 1,
+    PUT_PLUS = 2,
+    PUT_MINUS = 4,
+    BASE_2 = 8,
+    BASE_10 = 16,
+};
+
+static char * sitoa(char * buf, unsigned int num, int width, enum flag_itoa flags)
+{
+    unsigned int base;
+    if (flags & BASE_2)
+        base = 2;
+    else if (flags & BASE_10)
+        base = 10;
+    else
+        base = 16; // hex
+
+    char tmp[32];
+    char *p = tmp;
+    do {
+        int rem = num % base;
+        *p++ = (rem <= 9) ? (rem + '0') : (rem + 'a' - 0xA);
+    } while ((num /= base));
+    width -= p - tmp;
+    char fill = (flags & FILL_ZERO)? '0' : ' ';
+    while (0 <= --width) {
+        *(buf++) = fill;
+    }
+    if (flags & PUT_MINUS)
+        *(buf++) = '-';
+    else if (flags & PUT_PLUS)
+        *(buf++) = '+';
+    do
+        *(buf++) = *(--p);
+    while (tmp < p);
+    return buf;
+}
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+    char c;
+    const char *save = out;
+
+    while ((c  = *fmt++)) {
+        int width = 0;
+        enum flag_itoa flags = 0;
+        if (c != '%') {
+            *(out++) = c;
+            continue;
+        }
+    redo_spec:
+        c  = *fmt++;
+        switch (c) {
+        case '%':
+            *(out++) = c;
+            break;
+        case 'c':;
+            *(out++) = va_arg(ap, int);
+            break;
+        case 'd':;
+            int num = va_arg(ap, int);
+            if (num < 0) {
+                num = -num;
+                flags |= PUT_MINUS;
+            }
+            out = sitoa(out, num, width, flags | BASE_10);
+            break;
+        case 'u':
+            out = sitoa(out, va_arg(ap, unsigned int), width, flags | BASE_10);
+            break;
+        case 'x':
+            out = sitoa(out, va_arg(ap, unsigned int), width, flags); // hex need no flag
+            break;
+        case 'b':
+            out = sitoa(out, va_arg(ap, unsigned int), width, flags | BASE_2);
+            break;
+        case 's':;
+            const char *p  = va_arg(ap, const char *);
+            if (p) {
+                while (*p)
+                    *(out++) = *(p++);
+            }
+            break;
+        case 'm':; // I don't know what is m for;
+            const uint8_t *m  = va_arg(ap, const uint8_t *);
+            width = width < 64 ? width : 64; // buffer limited to 256!
+            if (m)
+                for (;;) {
+                    out = sitoa(out, *(m++), 2, FILL_ZERO);
+                    if (--width <= 0)
+                        break;
+                    *(out++) = ':';
+                }
+            break;
+        case '0':
+            if (!width)
+                flags |= FILL_ZERO;
+            // fall through
+        case '1'...'9':
+            width = width * 10 + c - '0';
+            goto redo_spec;
+        case '*':
+            width = va_arg(ap, unsigned int);
+            goto redo_spec;
+        case '+':
+            flags |= PUT_PLUS;
+            goto redo_spec;
+        case '\0':
+        default:
+            *(out++) = '?';
+        }
+        width = 0;
+    }
+    *out = '\0';
+    return out - save;  
+
+
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsprintf(out, fmt, ap);
+  va_end(ap); 
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
