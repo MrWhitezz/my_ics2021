364d8840bba3d65d6a226ae53af17ca1714f2c7e
tracer-ics2021 <tracer@njuics.org>
2021-10-17 00:42:17 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 2e5a36af46564d2899d110ec4ec810b9   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  00:42:17 up  4:00,  3 users,  load average: 0.45, 0.25, 0.26

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -28,6 +28,7 @@ void sdb_set_batch_mode();
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
+#include<string.h>
 static int difftest_port = 1234;
 
 static long load_img() {
