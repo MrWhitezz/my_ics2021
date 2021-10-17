7b3142a3fb59d1ccc603048102e96911d1cd886b
tracer-ics2021 <tracer@njuics.org>
2021-10-17 19:05:57 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 2e5a36af46564d2899d110ec4ec810b9   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  19:05:57 up 22:23,  3 users,  load average: 0.69, 0.34, 0.14

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -38,23 +38,23 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
+  if (CONFIG_MTRACE){
+    printf("Read Memory at: 0x%08x:\t%08x", addr, pmem_read(addr, len));
+  }
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   MUXDEF(CONFIG_DEVICE, return mmio_read(addr, len),
     panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
       addr, CONFIG_MBASE, CONFIG_MBASE + CONFIG_MSIZE, cpu.pc));
   // trace memory
-  if (CONFIG_MTRACE){
-    printf("Read Memory at: 0x%08x:\t%08x", addr, pmem_read(addr, len));
-  }
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+  if (CONFIG_MTRACE){
+    printf("Write Memory at: 0x%08x:\t%08x", addr, data);
+  }
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   MUXDEF(CONFIG_DEVICE, mmio_write(addr, len, data),
     panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
       addr, CONFIG_MBASE, CONFIG_MBASE + CONFIG_MSIZE, cpu.pc));
   // trace memory
-  if (CONFIG_MTRACE){
-    printf("Write Memory at: 0x%08x:\t%08x", addr, data);
-  }
 }
