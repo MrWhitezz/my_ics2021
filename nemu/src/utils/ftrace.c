264adb3717b181093fce56b55ba8a4ea93f4a540
tracer-ics2021 <tracer@njuics.org>
2021-10-19 23:30:17 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 9d13f6b70c9e4a0c82894c9ecc89b9e5   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  23:30:17 up  4:28,  4 users,  load average: 0.98, 1.03, 1.01

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -1,8 +1,30 @@
 #include<common.h>
+static char *buffer;
 void init_ftrace(const char *trace_file){
     if (trace_file == NULL){
         Log("No function trace elf file is given");
     }
     FILE* fp = fopen(trace_file, "rb");
     Assert(fp, "Cannot open '%s'", trace_file);
+
+    fseek(fp, 0, SEEK_END);
+    long size_elf = ftell(fp);
+
+    Log("The function trace file is %s, size = %ld", trace_file, size_elf);
+    buffer = calloc(1, size_elf + 1);
+    fseek(fp, 0, SEEK_SET);
+    // test for open file and read
+    if (!buffer) {
+        fclose(fp);
+        Log("memory alloc to function trace buffer fails");
+        assert(0);
+    }
+
+    if (1 != fread(buffer, size_elf, 1, fp)){
+        fclose(fp); free(buffer);
+        Log("entire read from function trace elf fails");
+    }
+
+    fclose(fp);
+
 }
