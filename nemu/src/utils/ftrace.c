33bd5d4aa44eee08d0fa84d28dbe1ea0de7e04af
tracer-ics2021 <tracer@njuics.org>
2021-10-19 23:20:49 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 9d13f6b70c9e4a0c82894c9ecc89b9e5   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  23:20:49 up  4:19,  4 users,  load average: 0.84, 0.98, 1.03

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -3,4 +3,6 @@ void init_ftrace(const char *trace_file){
     if (trace_file == NULL){
         Log("No function trace elf file is given");
     }
+    FILE* fp = fopen(trace_file, "rb");
+    Assert(fp, "Cannot open '%s'", trace_file);
 }
