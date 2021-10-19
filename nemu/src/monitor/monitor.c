6d5df5d4e75de89b4b1bc732403a2fac2c254918
tracer-ics2021 <tracer@njuics.org>
2021-10-19 23:02:34 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 9d13f6b70c9e4a0c82894c9ecc89b9e5   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  23:02:34 up  4:01,  4 users,  load average: 1.00, 1.01, 1.00

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -112,7 +112,7 @@ void init_monitor(int argc, char *argv[]) {
   long img_size = load_img();
 
   /* Initialize function trace, this will explain elf file*/
-  init_ftrace(trace_file_elf);
+  //init_ftrace(trace_file_elf);
 
   /* Initialize differential testing. */
   init_difftest(diff_so_file, img_size, difftest_port);
