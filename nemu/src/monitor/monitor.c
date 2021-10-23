a2c9ba50363346a9e1d7575c9c37d3446e703eda
tracer-ics2021 <tracer@njuics.org>
2021-10-23 16:22:57 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: a972f37452c24e2eb6131822795d3d14   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  16:22:57 up 2 days,  3:02,  6 users,  load average: 0.09, 0.16, 0.24

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -72,8 +72,8 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
-      case 'i': img_file = optarg; return optind - 1;
       case 'f': trace_file_elf = optarg; break; 
+      case 1: img_file = optarg; return optind - 1;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
         printf("\t-b,--batch              run with batch mode\n");
