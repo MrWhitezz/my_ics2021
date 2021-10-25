d7153c79e8ae12b6a3ffcd284e7cd1c5d85abd89
tracer-ics2021 <tracer@njuics.org>
2021-10-25 13:35:07 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: ac754d6b263d475792256a8320af3404   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  13:35:07 up 4 min,  1 user,  load average: 0.57, 0.28, 0.10

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,9 +5,9 @@
 void __am_timer_init() {
 }
 
-
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = inl(RTC_ADDR) + ((uint64_t)inl(RTC_ADDR + 4) << 32);
+  uptime->us = 0; //wrong
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
