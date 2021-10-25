aff073e34ac687a1bc35befa5463579c12ce2255
tracer-ics2021 <tracer@njuics.org>
2021-10-25 20:42:22 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: ac754d6b263d475792256a8320af3404   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  20:42:22 up  7:11,  3 users,  load average: 0.43, 0.36, 0.32

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,15 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  enum {_KEY_NONE = 0};
+  uint32_t key_info = inl(KBD_ADDR);
+  if (key_info == 0)
+    kbd->keydown = 0;
+  else 
+    kbd->keydown = 1;
+  
+  if (kbd->keydown == 1)
+    kbd->keycode = key_info;
+  else
+    kbd->keycode = AM_KEY_NONE;
 }
