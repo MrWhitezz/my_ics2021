1b7c7a3b9f1eb7732670f749fff0f7e72c7a02b4
tracer-ics2021 <tracer@njuics.org>
2021-10-27 15:15:10 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 710289ccaf65488a83219e97999ec030   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  15:15:11 up 18:10,  3 users,  load average: 0.58, 0.67, 0.63

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -3,7 +3,7 @@
 
 #define INSTR_LIST(f) f(add) f(sub) f(sll) f(slt) f(sltu) f(xor) f(srl) f(sra) f(or) f(and) \
 f(mul) f(mulu) f(mulhu) f(div) f(divu) f(rem) f(remu) \
-f(addi) f(sltiu) f(xori) f(ori) f(andi) f(slli) f(srli) f(srai) f(lui) f(auipc) \
+f(addi) f(slti) f(sltiu) f(xori) f(ori) f(andi) f(slli) f(srli) f(srai) f(lui) f(auipc) \
 f(jal) f(jalr) \
 f(lb) f(lh) f(lbu) f(lw) f(lhu) f(sb) f(sh) f(sw) \
 f(beq) f(bne) f(blt) f(bge) f(bltu) f(bgeu) \
