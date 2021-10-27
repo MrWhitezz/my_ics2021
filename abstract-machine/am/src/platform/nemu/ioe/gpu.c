6a95fbd69a021b0eb3b16550d318dd827984648f
tracer-ics2021 <tracer@njuics.org>
2021-10-27 11:48:16 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 710289ccaf65488a83219e97999ec030   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  11:48:16 up 14:43,  3 users,  load average: 0.20, 0.21, 0.27

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,16 +5,16 @@
 
 void __am_gpu_init() {
   //test
-  int i;
-  int w = inw(VGACTL_ADDR + 2);
-  int h = inw(VGACTL_ADDR);
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; ++i) fb[i] = i / 2;
-  outl(SYNC_ADDR, 1);
+  // int i;
+  // int w = inw(VGACTL_ADDR + 2);
+  // int h = inw(VGACTL_ADDR);
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < w * h; ++i) fb[i] = i / 2;
+  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  int wid = inw(VGACTL_ADDR);
+  int wid = inw(VGACTL_ADDR + 2);
   int hgt = inw(VGACTL_ADDR);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
@@ -27,7 +27,15 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
-
+  int wid = inw(VGACTL_ADDR + 2);
+  // int hgt = inw(VGACTL_ADDR);
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  uint32_t *color_buf = (uint32_t *)ctl->pixels;
+  for (int i = 0; i < w; ++i)
+    for (int j = 0; j < h; ++j){
+      *(fb + (j + y) * wid + (i + x)) = color_buf[i * w + j];
+    }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
