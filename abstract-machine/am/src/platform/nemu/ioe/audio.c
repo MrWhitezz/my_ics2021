9170f017cc2d21a2646893a0dcd3dd5646d3c4c8
tracer-ics2021 <tracer@njuics.org>
2021-10-31 15:31:51 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 0742c6b5e1ef4984a14713e4fd6cfb09   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  15:31:51 up  4:00,  4 users,  load average: 0.21, 0.38, 0.32

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -7,20 +7,51 @@
 #define AUDIO_SBUF_SIZE_ADDR (AUDIO_ADDR + 0x0c)
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
+#define AUDIO_SBUF_HEAD      (AUDIO_ADDR + 0X18)
 
 void __am_audio_init() {
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = false;// when finish, change it to true
+  cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  outl(AUDIO_INIT_ADDR, 1);
+  outl(AUDIO_FREQ_ADDR, ctrl->freq);
+  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
+}
+
+static void buf_write(uint8_t *buf, int len1){
+  int head = inl(AUDIO_SBUF_HEAD);
+  int bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
+  for (int i = 0; i < len1; ++i){
+    int offset = (head + i) % bufsize;
+    outl(AUDIO_SBUF_ADDR + offset, buf[i]);
+  }
+}
+
+static void audio_write(uint8_t *buf, int len){
+  int nwrite = 0;
+  int bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
+  while (nwrite < len){
+    int count = inl(AUDIO_COUNT_ADDR);
+    int remain = bufsize - count;
+    int write_tmp = len - nwrite;
+    if (write_tmp > remain) write_tmp = remain;
+    buf_write(buf + nwrite, write_tmp);
+    nwrite += write_tmp;
+    outl(AUDIO_COUNT_ADDR, count + write_tmp);
+  }
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  int len = ctl->buf.end - ctl->buf.start;
+  audio_write(ctl->buf.start, len);
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -9,18 +9,43 @@ enum {
   reg_sbuf_size,
   reg_init,
   reg_count,
+  reg_sbuf_head,
   nr_reg
 };
 
 SDL_AudioSpec s = {};
-
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
 
+static void audio_play(void *userdata, uint8_t *stream, int len){
+  int nread = len;
+  if (audio_base[reg_count] < len) nread = audio_base[reg_count];
+
+  int head = audio_base[reg_sbuf_head];
+  for (int i = 0; i < nread; ++i){
+    int offset = (head + i) % CONFIG_SB_SIZE; 
+    stream[i] = sbuf[offset];
+  }
+  audio_base[reg_sbuf_head] = (head + nread) % CONFIG_SB_SIZE;
+
+  if (len > nread){
+    memset(stream + nread, 0, len - nread);
+  }
+}
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
-  if (is_write == true){
-
+  if (is_write == true && audio_base[reg_init] != 0){
+    audio_base[reg_init] = 0;// init only once
+    s.format = AUDIO_S16SYS;
+    s.userdata = NULL;
+    s.freq = audio_base[reg_freq];
+    s.channels = audio_base[reg_channels];
+    s.samples = audio_base[reg_samples];
+    // TODO: write callback
+    s.callback = audio_play;
+    SDL_InitSubSystem(SDL_INIT_AUDIO);
+    SDL_OpenAudio(&s, NULL);
+    SDL_PauseAudio(0);
   }
 }
 
@@ -34,5 +59,7 @@ void init_audio() {
 #endif
 
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
+  audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
+  audio_base[reg_sbuf_head] = 0;
   add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
 }
