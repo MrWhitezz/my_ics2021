ae32b99ec74d3e27945ed0a2d48945265e9b069b
tracer-ics2021 <tracer@njuics.org>
2021-11-09 23:00:16 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: e2d805c1b3d9436b8921dc1c7bc68a47   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  23:00:16 up  1:40,  3 users,  load average: 0.68, 0.33, 0.31

--- nemu/src/isa/riscv32/instr/ctrlstat.h
+++ nemu/src/isa/riscv32/instr/ctrlstat.h
@@ -1,4 +1,4 @@
 def_EHelper(csrrw) {
-   rtl_addi(s, ddest, id_src2, 0);
-   rtl_addi(s, id_src2, id_src1, 0); // id_src2 is csr
+   rtl_addi(s, ddest, dsrc2, 0);
+   rtl_addi(s, dsrc2, dsrc1, 0); // dsrc2 is csr
 }
