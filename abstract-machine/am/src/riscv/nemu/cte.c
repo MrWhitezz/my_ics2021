32830c126ec8249926c3b0d4f8c6e181ae1bb26e
tracer-ics2021 <tracer@njuics.org>
2021-11-10 15:16:47 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: e2d805c1b3d9436b8921dc1c7bc68a47   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  15:16:48 up 17:56,  3 users,  load average: 0.43, 0.38, 0.37

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -16,10 +16,10 @@ Context* __am_irq_handle(Context *c) {
   }
   // test for Context
   for (int i = 0; i < 32; ++i)
-      printf("R[%d] = 0x%x\t", i, c->gpr[i]);
-  printf("SR[cause] = 0x%x\n", c->mcause);
-  printf("SR[epc] = 0x%x\n", c->mepc);
-  printf("SR[status] = 0x%x\n", c->mstatus);
+      printf("R[%d] = 0x%d\t", i, c->gpr[i]);
+  printf("SR[cause] = 0x%d\n", c->mcause);
+  printf("SR[epc] = 0x%d\n", c->mepc);
+  printf("SR[status] = 0x%d\n", c->mstatus);
 
   return c;
 }
