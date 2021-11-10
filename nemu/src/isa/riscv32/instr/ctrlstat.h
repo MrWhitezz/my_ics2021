617cdd74f8d3778e808562399000d8b2206b4505
tracer-ics2021 <tracer@njuics.org>
2021-11-10 14:24:48 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: e2d805c1b3d9436b8921dc1c7bc68a47   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  14:24:49 up 17:04,  3 users,  load average: 0.31, 0.38, 0.40

--- nemu/src/isa/riscv32/instr/ctrlstat.h
+++ nemu/src/isa/riscv32/instr/ctrlstat.h
@@ -3,7 +3,12 @@ def_EHelper(csrrw) {
    rtl_addi(s, dsrc2, dsrc1, 0); // dsrc2 is csr
 }
 
+def_EHelper(csrrs) {
+   rtl_addi(s, ddest, dsrc2, 0);
+   rtl_or(s, dsrc2, dsrc2, dsrc1); // set the bits in rs1 which is high
+}
+
 def_EHelper(ecall) {
-   vaddr_t target = isa_raise_intr(-1, s->pc);// not sure which pc
+   vaddr_t target = isa_raise_intr(-1, s->dnpc);// not sure which pc
    rtl_j(s, target);
 }
