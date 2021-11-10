bff5d16e1283d5ea310e4f64a831c2eda7c7b145
tracer-ics2021 <tracer@njuics.org>
2021-11-10 16:48:29 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: e2d805c1b3d9436b8921dc1c7bc68a47   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  16:48:29 up 19:28,  3 users,  load average: 0.60, 0.49, 0.35

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD: /*printf("Hit the good yield!\n");*/ break;
+    case EVENT_YIELD: printf("Hit the good yield!\n"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
