99e0f1492ab8321f2b6e97f2e51dc4c9b5636ebc
tracer-ics2021 <tracer@njuics.org>
2021-11-13 14:58:25 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 47dac783b9c946129d5482609213afc1   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  14:58:25 up  3:16,  1 user,  load average: 0.61, 0.33, 0.29

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -8,8 +8,10 @@
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
 #endif
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t get_ramdisk_size();
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
+static uintptr_t loader(PCB *pcb, const char *filename) { // temporarily ignore pcd and filename
   TODO();
   return 0;
 }
