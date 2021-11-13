7cd333873596b9a5d97b645aaaa1a9e3056d4a6e
tracer-ics2021 <tracer@njuics.org>
2021-11-13 20:34:19 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 47dac783b9c946129d5482609213afc1   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  20:34:19 up  8:52,  3 users,  load average: 0.47, 0.34, 0.36

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,7 +8,7 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 0xb: ev.event = EVENT_YIELD; break;
+      case 0xc: ev.event = EVENT_YIELD; break;
       default: ev.event = EVENT_ERROR; printf("EVENT_ERROR!\n"); break;
     }
 
