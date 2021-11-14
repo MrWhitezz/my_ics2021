a0b37b33a64fa1b0e0afe9e66e32ddf4aebabca3
tracer-ics2021 <tracer@njuics.org>
2021-11-14 12:32:42 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 0ceb74a563304e918ef5f41be1141588   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  12:32:42 up  1:34,  3 users,  load average: 0.54, 0.37, 0.25

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -11,7 +11,7 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 0xb: ev.event = EVENT_YIELD;   break;
+      case  -1: ev.event = EVENT_YIELD;   break;
       case 0x1: ev.event = EVENT_SYSCALL; break;
       default:  ev.event = EVENT_ERROR;    break;
     }
@@ -40,7 +40,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 }
 
 void yield() {
-  asm volatile("li a7, 0xb; ecall");
+  asm volatile("li a7, -1; ecall");
 }
 
 bool ienabled() {
