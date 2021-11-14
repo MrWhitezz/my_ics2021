58d6c897cfbbb94c3e20157e59365700406c8888
tracer-ics2021 <tracer@njuics.org>
2021-11-14 12:08:23 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 0ceb74a563304e918ef5f41be1141588   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  12:08:23 up  1:10,  3 users,  load average: 0.41, 0.27, 0.29

--- nemu/src/isa/riscv32/instr/ctrlstat.h
+++ nemu/src/isa/riscv32/instr/ctrlstat.h
@@ -12,7 +12,8 @@ def_EHelper(ecall) {
    #ifdef CONFIG_ETRACE
       printf("Exception given: \n");
    #endif
-   vaddr_t target = isa_raise_intr(cpu.gpr[17]._32, s->pc);
+   // vaddr_t target = isa_raise_intr(cpu.gpr[17]._32, s->pc);
+   vaddr_t target = isa_raise_intr(0xb, s->pc); // WRONG!!!
    // cpu.mepc = s->pc;
    // vaddr_t target = cpu.mtvec;
    // cpu.gpr[1]._32 = 0x114514;//bug
