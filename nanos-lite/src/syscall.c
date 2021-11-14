ea13566e5e908ec969bac109f80bc5f96b69439c
tracer-ics2021 <tracer@njuics.org>
2021-11-14 14:51:39 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 0ceb74a563304e918ef5f41be1141588   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  14:51:39 up  3:53,  3 users,  load average: 0.57, 0.34, 0.15

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,11 +6,16 @@ static void sys_yield(Context *c){
   c->GPRx = 0;
 }
 
+static void sys_exit(Context *c){
+  halt(0);
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
+    case EVENT_NULL:  sys_exit(c);  break;
     case EVENT_YIELD: sys_yield(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
