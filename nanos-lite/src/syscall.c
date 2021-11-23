5bd421649d234945ff7e0f504acdbcc14c6d9371
tracer-ics2021 <tracer@njuics.org>
2021-11-23 20:57:34 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 909a8be02df8494ab4d1e2840cbfc953   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  20:57:34 up  2:28,  1 user,  load average: 0.40, 0.37, 0.36

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,24 +1,11 @@
 #include <common.h>
 #include "syscall.h"
 
-enum {
-  SYS_exit, SYS_yield,
-  SYS_open, SYS_read, SYS_write,
-  SYS_kill,
-  SYS_getpid,
-  SYS_close,
-  SYS_lseek,
-  SYS_brk,
-  SYS_fstat,
-  SYS_time,
-  SYS_signal,
-  SYS_execve,
-  SYS_fork,
-  SYS_link,
-  SYS_unlink,
-  SYS_wait,
-  SYS_times, SYS_gettimeofday
-};
+int fs_open(const char *pathname, int flags, int mode);
+int fs_close(int fd);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
 
 static void sys_yield(Context *c){
   yield();
@@ -29,13 +16,31 @@ static void sys_exit(Context *c){
   halt(c->GPR2);
 }
 
+static void sys_open(Context *c, const char *filename, int flags, int mode){
+  int fd = fs_open(filename, flags, mode);
+  c->GPRx = fd;
+}
+
+static void sys_read(Context *c, int fd, void *buf, size_t len){
+  c->GPRx = fs_read(fd, buf, len);
+}
+
 static void sys_write(Context *c, int fd, void *buf, size_t count) { // bug here
-  if (fd == 1 || fd == 2) {
-    // printf("count = %d\n", count);
-    for (int i = 0; i < count; ++i)
-      {putch(((char *)buf)[i]);}
-  }
-  c->GPRx = count;
+  // if (fd == 1 || fd == 2) {
+  //   // printf("count = %d\n", count);
+  //   for (int i = 0; i < count; ++i)
+  //     {putch(((char *)buf)[i]);}
+  // }
+  // c->GPRx = count;
+  c->GPRx = fs_write(fd, buf, count);
+}
+
+static void sys_close(Context *c, int fd){
+  c->GPRx = fs_close(fd);
+}
+
+static void sys_lseek(Context *c, int fd, size_t offset, int whence){
+  c->GPRx = fs_lseek(fd, offset, whence);
 }
 
 static void sys_brk(Context *c) {
@@ -46,8 +51,12 @@ void strace(uintptr_t a7){
   switch (a7){
     case SYS_exit:  printf("System Call: exit\n");  break;
     case SYS_yield: printf("System Call: yield\n"); break;
+    case SYS_open:  printf("System Call: open\n");  break;
+    case SYS_read:  printf("System Call: read\n");  break;
     case SYS_write: printf("System Call: write\n"); break;
-    case SYS_brk:   printf("System Call: brk\n"); break;
+    case SYS_close: printf("System Call: close\n"); break;
+    case SYS_lseek: printf("System Call: lseek\n"); break;
+    case SYS_brk:   printf("System Call: brk\n");   break;
     
 
     default: printf("Unknown System Call\n");
@@ -74,7 +83,11 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case SYS_exit:  sys_exit(c);  break;
     case SYS_yield: sys_yield(c); break;
+    case SYS_open:  sys_open(c, (char *)a[1], a[2], a[3]); break;
+    case SYS_read:  sys_read(c, a[1], (void *)a[2], a[3]); break;
     case SYS_write: sys_write(c, a[1], (void *)a[2], a[3]); break;
+    case SYS_close: sys_close(c, a[1]); break;
+    case SYS_lseek: sys_lseek(c, a[1], a[2], a[3]); break;
     case SYS_brk:   sys_brk(c);   break;
     case -1       : printf("Hit the Strange yield!\n"); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
