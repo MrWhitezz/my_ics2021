f8307f5d19ada5bc06d951f2d51f7468d5c4d602
tracer-ics2021 <tracer@njuics.org>
2021-11-24 11:58:26 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 8de51956052c441b9c2382c3909da589   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  11:58:26 up  1:47,  1 user,  load average: 0.93, 0.66, 0.57

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -50,7 +50,7 @@ static void sys_gettimeofday(Context *c, struct timeval *tv, struct timezone *tz
   tv->tv_sec = us / 1000000ULL;
   tv->tv_usec = us % 1000000ULL;
   if (tv->tv_sec != 0){
-    printf("NBBBB\n");
+    printf("tv_sec is read as %d\n", (uint32_t)tv->tv_sec);
   }
   // printf("sec is read as %x\n", tv->tv_sec);
   // printf("usec is read as %x\n", tv->tv_usec);
