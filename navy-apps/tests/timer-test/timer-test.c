eb1ab3e4d1b894640500dc88b7508fe88da67eff
tracer-ics2021 <tracer@njuics.org>
2021-11-24 12:03:09 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 8de51956052c441b9c2382c3909da589   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  12:03:09 up  1:52,  1 user,  load average: 0.76, 0.69, 0.60

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -8,11 +8,11 @@ int main() {
  
   gettimeofday(&end, NULL);
   while(1){
+        printf("tv_sec = %d\n", end.tv_sec);
+        printf("tv_usec = %d\n", (__uint32_t)end.tv_usec);
     gettimeofday(&end, NULL);
     if (((end.tv_sec * 1000000 + end.tv_usec) 
       - (start.tv_sec * 1000000 + start.tv_usec)) >= 500000){
-        printf("tv_sec = %d\n", end.tv_sec);
-        printf("tv_usec = %d\n", (__uint32_t)end.tv_usec);
         printf("%d * 0.5 seconds passed\n", ++i);
         gettimeofday(&start, NULL);
     }
