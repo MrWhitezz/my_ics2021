a9afad319fe436a72d04181a83b5f880a72d6092
tracer-ics2021 <tracer@njuics.org>
2021-11-24 14:08:06 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 8de51956052c441b9c2382c3909da589   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  14:08:06 up  3:57,  5 users,  load average: 0.17, 0.39, 0.34

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -4,23 +4,12 @@
 uint32_t NDL_GetTicks();
 int main() {
   int i = 0;
-  // struct timeval start, end;
-  // struct timezone tz;
-  // gettimeofday(&start, NULL);
-  // gettimeofday(&end, NULL);
   uint32_t beg, end;
   beg = NDL_GetTicks();
   while(1){
-        // printf("tv_sec = %d\n", end.tv_sec);
-        // printf("tv_usec = %d\n", end.tv_usec);
-        //????????????????????????????
-    // gettimeofday(&end, NULL);
     end = NDL_GetTicks();
-    // if (((end.tv_sec * 1000000 + end.tv_usec) 
-    //   - (start.tv_sec * 1000000 + start.tv_usec)) >= 500000){
     if (end - beg >= 500) {
         printf("%d * 0.5 seconds passed\n", ++i);
-        // gettimeofday(&start, NULL);
         beg = end;
     }
   }
