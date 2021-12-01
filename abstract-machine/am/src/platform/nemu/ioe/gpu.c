ef3a8e7b954e14ec55fb6386e0984d4099b769cf
tracer-ics2021 <tracer@njuics.org>
2021-12-01 21:24:08 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 49c1aadee85344a9a16a6d4eedd2c224   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  21:24:08 up 11:17,  3 users,  load average: 0.40, 0.54, 0.60

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -27,14 +27,14 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
-  int wid = inw(VGACTL_ADDR + 2);
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-  uint32_t *color_buf = (uint32_t *)ctl->pixels;
-  for (int j = 0; j < h; ++j)
-    for (int i = 0; i < w; ++i){
-      *(fb + (y + j) * wid + (x + i)) = color_buf[j * w + i];
-    }
+  // int wid = inw(VGACTL_ADDR + 2);
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  // uint32_t *color_buf = (uint32_t *)ctl->pixels;
+  // for (int j = 0; j < h; ++j)
+  //   for (int i = 0; i < w; ++i){
+  //     *(fb + (y + j) * wid + (x + i)) = color_buf[j * w + i];
+  //   }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
