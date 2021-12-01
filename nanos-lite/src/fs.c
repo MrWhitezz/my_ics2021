370deab787db39854e6dcef8cabeb77725f4ec77
tracer-ics2021 <tracer@njuics.org>
2021-12-01 21:47:28 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: 49c1aadee85344a9a16a6d4eedd2c224   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  21:47:29 up 11:40,  3 users,  load average: 1.01, 1.13, 0.90

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -53,7 +53,8 @@ int fs_open(const char *pathname, int flags, int mode){
   for (int i = 0; i < LENGTH(file_table); ++i){
     if (strcmp(pathname, file_table[i].name) == 0){
       file_table[i].open_offset = 0;
-      printf("Successly open file %s\n", pathname);
+      if (i != FD_EVENT)
+        printf("Successly open file %s\n", pathname);
       return i;
     } 
   }
