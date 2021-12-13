36071ad9d7ec7e5ea976424504c331247a7bdba3
tracer-ics2021 <tracer@njuics.org>
2021-12-13 20:05:27 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: b3f76de78d7f4a87aa4276137fd3e29e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  20:05:27 up  1:54,  1 user,  load average: 1.12, 1.07, 0.99

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -17,6 +17,7 @@ Context* __am_irq_handle(Context *c) {
   // printf("SR[cause] = 0x%x\n", c->mcause);
   // printf("SR[epc] = 0x%x\n", c->mepc);
   // printf("SR[status] = 0x%d\n", c->mstatus);
+    printf("cte happened\n");
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
