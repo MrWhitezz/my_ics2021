fbdb6322b8df1be5df7905cc5cdda7bd80639c8a
tracer-ics2021 <tracer@njuics.org>
2021-12-13 20:03:08 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: b3f76de78d7f4a87aa4276137fd3e29e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  20:03:08 up  1:52,  1 user,  load average: 0.98, 1.01, 0.95

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -160,7 +160,7 @@ void init_proc() {
   // context_uload(&pcb[0], "/bin/hello");
   argv_pal[0] = exec_arg;
   // context_kload(&pcb[1], hello_fun, (void *)0x2);
-  context_uload(&pcb[1], "/bin/nterm", argv_pal, NULL);
+  context_uload(&pcb[1], "/bin/nterm", NULL, NULL);
   switch_boot_pcb();
   printf("What happened\n");
   yield();
