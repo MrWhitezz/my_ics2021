21e1fbf44e3f4dd2424f55ebc41297109c4482c5
tracer-ics2021 <tracer@njuics.org>
2021-12-13 23:12:10 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: b3f76de78d7f4a87aa4276137fd3e29e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  23:12:10 up  5:01,  2 users,  load average: 0.31, 0.29, 0.31

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -4,13 +4,34 @@
 
 extern char _pmem_start;
 #define PMEM_SIZE (128 * 1024 * 1024)
+#define PMEM_BEG  ((uintptr_t)&_pmem_start)
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE) // 0x80000000 to 0x88000000
+#define MMIO_BASE 0xa0000000
+#define MMIO_END  (MMIO_BASE + 0x1000)
+#define FB_ADDR   (MMIO_BASE + 0x1000000)
+#define FB_END    (FB_ADDR   + 0x200000)
 
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   return MEM_RET_FAIL;
 }
 
+bool is_in_pmem(vaddr_t va_beg, vaddr_t va_end){
+  return (va_beg >= PMEM_BEG && va_end <= PMEM_END);
+}
+
+bool is_in_mmio(vaddr_t va_beg, vaddr_t va_end){
+  return (va_beg >= MMIO_BASE && va_end <= MMIO_END);
+}
+
+bool is_in_FB(vaddr_t va_beg, vaddr_t va_end){
+  return (va_beg >= FB_ADDR && va_end <= FB_END);
+}
+
 int isa_mmu_check(vaddr_t vaddr, int len, int type){
+  vaddr_t va_end = vaddr + len;
+  if (is_in_pmem(vaddr, va_end) || is_in_mmio(vaddr, va_end) || is_in_FB(vaddr, va_end)){
+    
+  }
   return MMU_DIRECT;
 }
