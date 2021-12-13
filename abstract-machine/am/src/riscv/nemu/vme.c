52606894f6aa5fddc8aceb5a8f256bc3e7d56d44
tracer-ics2021 <tracer@njuics.org>
2021-12-13 20:14:51 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: b3f76de78d7f4a87aa4276137fd3e29e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  20:14:51 up  2:04,  1 user,  load average: 1.29, 1.13, 1.04

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -15,6 +15,8 @@ static Area segments[] = {      // Kernel memory mappings
 
 static inline void set_satp(void *pdir) {
   uintptr_t mode = 1ul << (__riscv_xlen - 1);
+  printf("__riscv_xlen = %d\n", __riscv_xlen);
+  printf("set satp as %x\n", (mode | ((uintptr_t)pdir >> 12)));
   asm volatile("csrw satp, %0" : : "r"(mode | ((uintptr_t)pdir >> 12)));
 }
 
