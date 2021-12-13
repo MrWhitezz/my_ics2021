0d37ab718a24f97655d6233b5174e59dd6ab2e8e
tracer-ics2021 <tracer@njuics.org>
2021-12-13 22:51:51 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: b3f76de78d7f4a87aa4276137fd3e29e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  22:51:52 up  4:41,  2 users,  load average: 0.45, 0.36, 0.30

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -1,16 +1,30 @@
 #include <isa.h>
 #include <memory/paddr.h>
 
+#define mode_mask 0x80000000
+
 word_t vaddr_ifetch(vaddr_t addr, int len) {
+  uint32_t satp = cpu.satp;
+  if ((satp & mode_mask)){
+    assert(0);
+  }
   return paddr_read(addr, len);
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
   // int mm_check = isa_mmu_check(vaddr_t, len, MEM_TYPE_READ);
     // printf("__riscv_xlen = %d\n", 32);
+  uint32_t satp = cpu.satp;
+  if ((satp & mode_mask)){
+    assert(0);
+  }
   return paddr_read(addr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
+  uint32_t satp = cpu.satp;
+  if ((satp & mode_mask)){
+    assert(0);
+  }
   paddr_write(addr, len, data);
 }
