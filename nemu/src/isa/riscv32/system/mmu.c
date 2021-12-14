5482fc025d3670df383f0700e9140831702e3c64
tracer-ics2021 <tracer@njuics.org>
2021-12-14 13:26:05 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: b3f76de78d7f4a87aa4276137fd3e29e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  13:26:05 up 19:15,  2 users,  load average: 0.74, 0.62, 0.49

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -10,8 +10,31 @@
 #define FB_ADDR   (MMIO_BASE + 0x1000000)
 #define FB_END    (FB_ADDR   + 0x200000)
 
+#define SATP_PPN_MASK 0xfffff
+#define mode_mask     0x80000000
+#define PTESIZE       4
+#define exp2(x)       (1 << (x))
+
+vaddr va_tmp;
+paddr pa_tmp;
+pte pte1, pte2;
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
+  uint32_t satp_ppn = cpu.satp & SATP_PPN_MASK;
+  assert(cpu.satp & mode_mask);   assert(PAGE_SIZE == 4096);
+  va_tmp.vaddr_.val = vaddr;
+
+  paddr_t pte1_addr = (satp_ppn * PAGE_SIZE) + (va_tmp.vaddr_.va.vpn1 * PTESIZE);
+  pte1.pte_.val = paddr_read(pte1_addr, PTESIZE);
+  assert(pte1.pte_.pte.V == 1);
+  paddr_t pte2_addr = (pte1.pte_.pte.ppn1 * exp2(10) + pte1.pte_.pte.ppn0) * PAGE_SIZE + va_tmp.vaddr_.va.vpn0 * PTESIZE;
+  pte2.pte_.val = paddr_read(pte2_addr, PTESIZE);
+  assert(pte2.pte_.pte.V == 1);
+
+  paddr_t pa = ((pte2.pte_.pte.ppn1 * exp2(10) + pte2.pte_.pte.ppn0) * PAGE_SIZE) + va_tmp.vaddr_.va.page_offset;
+
+  return pa;
+
   assert(0);
   return MEM_RET_FAIL;
 }
