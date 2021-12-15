44c5ee20dc7cbccfad187a124fbb057e575fcd7b
tracer-ics2021 <tracer@njuics.org>
2021-12-15 20:38:27 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: b3f76de78d7f4a87aa4276137fd3e29e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  20:38:27 up 2 days,  2:27,  3 users,  load average: 0.50, 0.91, 0.76

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -190,7 +190,7 @@ Context* schedule(Context *prev) {
   current->cp = prev;
 
   current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-  current = &pcb[1];
+  // current = &pcb[1];
 
   assert(current->cp != NULL);
 
