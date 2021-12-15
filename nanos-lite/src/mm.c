54fe8f9146dcefcd4eeeb5ff9f1f2c5a3abd7184
tracer-ics2021 <tracer@njuics.org>
2021-12-15 19:57:44 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: b3f76de78d7f4a87aa4276137fd3e29e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  19:57:44 up 2 days,  1:47,  3 users,  load average: 0.68, 0.64, 0.56

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -29,8 +29,8 @@ int mm_brk(uintptr_t brk) {
   void *p_page = pg_alloc(nr_page);
   void *v_page = (void *)(ROUNDUP(current->max_brk, PGSIZE));
   printf("v_page + nrpage * PGSIZE = %x\n", (uintptr_t)(v_page + nr_page * PGSIZE));
-  printf("(current->max_brk + brk) / PGSIZE) = %x\n", (current->max_brk + brk) / PGSIZE);
-  assert((uintptr_t)(v_page + nr_page * PGSIZE) / PGSIZE == (current->max_brk + brk) / PGSIZE);
+  printf("(current->max_brk + brk)) = %x\n", (current->max_brk + brk));
+  assert((uintptr_t)(v_page + (nr_page - 1) * PGSIZE) / PGSIZE == (current->max_brk + brk) / PGSIZE);
   for (int i = 0; i < nr_page; ++i){
     map(&current->as, v_page + i * PGSIZE, p_page + i * PGSIZE, MMAP_READ | MMAP_WRITE);
   }
