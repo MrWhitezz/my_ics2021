b4c71476d4e4b17d6d5f699cad7aa1a6449d03d8
tracer-ics2021 <tracer@njuics.org>
2021-12-16 11:51:59 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: b3f76de78d7f4a87aa4276137fd3e29e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  11:51:59 up 2 days, 17:41,  3 users,  load average: 0.35, 0.46, 0.38

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -12,7 +12,10 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    */
   cpu.mepc = epc;
   cpu.mcause = NO;
+
   uint32_t mie = cpu.mstatus & MASK_MIE; 
+  if (mie != 0)
+    printf("cpu.mstatus = %x\n", cpu.mstatus);
   cpu.mstatus = (cpu.mstatus & UNMASK_M) + (((cpu.mstatus & MASK_MIE) != 0) ? MASK_MPIE : 0);
   assert((cpu.mstatus & MASK_MIE) == 0);
   assert(((cpu.mstatus & MASK_MPIE) == 0) == (mie == 0));
@@ -22,8 +25,6 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 }
 
 word_t isa_query_intr() {
-  if (cpu.INTR == true)
-    printf("cpu.mstatus = %x\n", cpu.mstatus);
   if (((cpu.mstatus & MASK_MIE) != 0) && cpu.INTR == true){
     cpu.INTR = false;
     return IRQ_TIMER;
