e0d57db1a747b8fb4d2b47ea6a230aef1a4588e1
tracer-ics2021 <tracer@njuics.org>
2021-12-16 11:48:45 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: b3f76de78d7f4a87aa4276137fd3e29e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  11:48:45 up 2 days, 17:38,  3 users,  load average: 0.73, 0.50, 0.37

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -22,7 +22,8 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 }
 
 word_t isa_query_intr() {
-  printf("cpu.mstatus = %x\n", cpu.mstatus);
+  if (cpu.INTR == true)
+    printf("cpu.mstatus = %x\n", cpu.mstatus);
   if (((cpu.mstatus & MASK_MIE) != 0) && cpu.INTR == true){
     cpu.INTR = false;
     return IRQ_TIMER;
