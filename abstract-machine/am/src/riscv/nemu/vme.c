1aaea477dba71776e19f795704e0290fd9b903e6
tracer-ics2021 <tracer@njuics.org>
2021-12-16 11:54:47 +0800

    >  compile 201180032    Static hostname: xyq-Lenovo-Legion-Y7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: aa02ee5847d5464f911c0d7dc9645b39            Boot ID: b3f76de78d7f4a87aa4276137fd3e29e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  11:54:47 up 2 days, 17:44,  3 users,  load average: 0.48, 0.52, 0.42

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -145,8 +145,8 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
     pte1.pte_.pte.ppn1 = pte_addr_tmp.paddr_.pa.ppn1;
     assert(pte_addr_tmp.paddr_.pa.page_offset == 0);
     *pte1_addr = pte1.pte_.val;
-    printf("va: %x pa: %x\n", va, pa);
-    printf("load level 1 page table entry at %p value %x >> 2\n", pte1_addr, *pte1_addr << 2);
+    // printf("va: %x pa: %x\n", va, pa);
+    // printf("load level 1 page table entry at %p value %x >> 2\n", pte1_addr, *pte1_addr << 2);
   }
   // printf("level 1 page table entry at %p value %x >> 2\n", pte1_addr, *pte1_addr << 2);
   assert(*pte1_addr == pte1.pte_.val);
